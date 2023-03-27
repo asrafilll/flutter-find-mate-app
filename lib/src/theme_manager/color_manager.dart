4a69c7fa6f6487854413fc974f4fce25dd0f47ac
asrafilll <asrafilr@gmail.com>
2023-03-27 14:06:42 +0700

    Added ColorManager

---
+++ lib/src/theme_manager/color_manager.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primaryColor = HexColor.fromHex('#03091C');
+  static Color secondaryColor = HexColor.fromHex('#262F4B');
+  static Color whiteColor = HexColor.fromHex('#FFFFFF');
+  static Color pinkColor = HexColor.fromHex('#EA887B');
+  static Color purpleColor = HexColor.fromHex('#CE92E3');
+  static Color black60Color = HexColor.fromHex('#737D9C');
+  static Color blackColor = HexColor.fromHex('#5B6279');
+  static Color greyColor = HexColor.fromHex('#D9D9D9');
+  static Color grey60Color = HexColor.fromHex('#A9A9AC');
+}
+
+extension HexColor on Color {
+  static Color fromHex(String hexColorString) {
+    hexColorString = hexColorString.replaceAll('#', '');
+    if (hexColorString.length == 6) {
+      hexColorString = 'FF$hexColorString';
+    }
+    return Color(int.parse(hexColorString, radix: 16));
+  }
+}
