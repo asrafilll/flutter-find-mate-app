6144d803effe352e08eb9480c7bd245a6f4c3952
asrafilll <asrafilr@gmail.com>
2023-03-27 13:49:42 +0700

    Create AssetManager for Images and Icons

---
+++ lib/src/theme_manager/asset_manager.dart
@@ -0,0 +1,27 @@
+class AssetManager {
+  static const String _assetPath = 'assets/images';
+
+  // ------------------- IMAGES -------------------
+  static const String heroImg = '$_assetPath/hero_image.png';
+  static const String glassCardImg = '$_assetPath/glass_card_image.png';
+  static const String hobby1Img = '$_assetPath/hobby1_image.png';
+  static const String hobby2Img = '$_assetPath/hobby2_image.png';
+  static const String logoImg = '$_assetPath/logo_image.png';
+  static const String peopleLove1Img = '$_assetPath/people_love1_image.png';
+  static const String peopleLove2Img = '$_assetPath/people_love2_image.png';
+  static const String peopleLove3Img = '$_assetPath/people_love3_image.png';
+  static const String userImg = '$_assetPath/user_image.png';
+
+  // ------------------- ICONS -------------------
+
+  static const String addColorIcon = '$_assetPath/icon_add_color.png';
+  static const String arrowLeftIcon = '$_assetPath/icon_arrow_left.png';
+  static const String closeCircleIcon = '$_assetPath/icon_close_circle.png';
+  static const String closeIcon = '$_assetPath/icon_close.png';
+  static const String favoriteIcon = '$_assetPath/icon_favorite.png';
+  static const String loveIcon = '$_assetPath/icon_love.png';
+  static const String notificationIcon = '$_assetPath/icon_notification.png';
+  static const String iconProfile = '$_assetPath/icon_profile.png';
+  static const String searchIcon = '$_assetPath/icon_search.png';
+  static const String profileIcon = '$_assetPath/profile_icon.png';
+}
