f0cd46617ca9c56fe171ca684e8d03e008b87a58
asrafilll <asrafilr@gmail.com>
2023-03-29 15:50:12 +0700

    Image Picker Utils

--- lib/src/common_widgets/upload_photo_widget.dart
+++ lib/src/common_widgets/upload_photo_widget.dart
@@ -1,11 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_saranghae/src/theme_manager/asset_manager.dart';
 import 'package:flutter_saranghae/src/theme_manager/color_manager.dart';
 import 'package:flutter_saranghae/src/theme_manager/values_manager.dart';
 
 class UploadPhotoWidget extends StatelessWidget {
-  const UploadPhotoWidget({super.key});
+  const UploadPhotoWidget({
+    super.key,
+    required this.image,
+  });
 
+  final File? image;
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -13,21 +19,24 @@ class UploadPhotoWidget extends StatelessWidget {
       clipBehavior: Clip.none,
       children: [
         Container(
-          width: 180.0,
-          height: 180.0,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            image: const DecorationImage(
-              image: AssetImage(
-                AssetManager.profileIcon,
-              ),
+            width: 180.0,
+            height: 180.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                  width: AppSize.s8,
+                  color: ColorManager.secondaryColor,
+                  strokeAlign: BorderSide.strokeAlignOutside),
             ),
-            border: Border.all(
-              width: AppSize.s8,
-              color: ColorManager.secondaryColor,
-            ),
-          ),
-        ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: image != null
+                  ? Image(
+                      image: FileImage(image!),
+                      fit: BoxFit.cover,
+                    )
+                  : Image.asset(AssetManager.profileIcon),
+            )),
         Positioned(
           bottom: -25.0,
           child: SizedBox(
--- lib/src/features/authentication/presentation/sign_up_upload_photo_screen.dart
+++ lib/src/features/authentication/presentation/sign_up_upload_photo_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_saranghae/src/common_widgets/custom_button_widget.dart';
 import 'package:flutter_saranghae/src/common_widgets/custom_text_button_widget.dart';
@@ -7,10 +9,28 @@ import 'package:flutter_saranghae/src/features/likes_you/presentation/explore_pe
 import 'package:flutter_saranghae/src/theme_manager/font_manager.dart';
 import 'package:flutter_saranghae/src/theme_manager/textstyle_manager.dart';
 import 'package:flutter_saranghae/src/theme_manager/values_manager.dart';
+import 'package:flutter_saranghae/src/utils/image_picker_utils.dart';
 
-class SignUpUploadPhotoScreen extends StatelessWidget {
+class SignUpUploadPhotoScreen extends StatefulWidget {
   static const String routeName = '/sign-up-upload-photo';
   const SignUpUploadPhotoScreen({super.key});
+
+  @override
+  State<SignUpUploadPhotoScreen> createState() =>
+      _SignUpUploadPhotoScreenState();
+}
+
+class _SignUpUploadPhotoScreenState extends State<SignUpUploadPhotoScreen> {
+  File? image;
+
+  void getImageProfile(GetImageFrom source) async {
+    final result = await ImagePickerUtil.getImage(source);
+    if (result != null) {
+      setState(() {
+        image = File(result.path);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +45,45 @@ class SignUpUploadPhotoScreen extends StatelessWidget {
             children: [
               const LogoAndTaglineWidget(),
               const SizedBox(height: AppSize.s50),
-              const UploadPhotoWidget(),
+              InkWell(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        padding: const EdgeInsets.all(AppPadding.p32),
+                        margin: const EdgeInsets.only(bottom: 32.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                getImageProfile(GetImageFrom.camera);
+                              },
+                              icon: const Icon(
+                                Icons.camera,
+                                size: 48.0,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                getImageProfile(GetImageFrom.gallery);
+                              },
+                              icon: const Icon(
+                                Icons.photo,
+                                size: 48.0,
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: UploadPhotoWidget(
+                  image: image,
+                ),
+              ),
               const SizedBox(height: 53.0),
               Text(
                 'Andi Mania',
